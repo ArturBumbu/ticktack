7cf741decbf03d095a7708dbd735f0a5e9ce6c80
Artur <arturbumbu@gmail.com>
2016-06-14 20:22:41 +0100

    ComputerMover - will generate next computer position based on the game state. Checking if a generated position is valid.

---
+++ src/main/java/com/smartfocus/ticktack/ComputerMover.java
@@ -0,0 +1,16 @@
+package com.smartfocus.ticktack;
+
+/**
+ * Created by arthur on 14/06/16.
+ */
+public class ComputerMover {
+    private final GameBoard board;
+
+    public ComputerMover(GameBoard board) {
+        this.board = board;
+    }
+
+    public boolean isValid(int row, int col) {
+        return false;
+    }
+}
---
+++ src/test/java/com/smartfocus/ticktack/ComputerMoverTest.java
@@ -0,0 +1,57 @@
+package com.smartfocus.ticktack;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by arthur on 14/06/16.
+ */
+public class ComputerMoverTest {
+
+    private File file;
+    private GameBoard board;
+    ComputerMover computerMover;
+
+    @Before
+    public void setup() throws IOException {
+        file = createTestFile();
+        FileInputStream inputStream = new FileInputStream(file);
+        FileOutputStream fileOutputStream = new FileOutputStream(file);
+        InputOutputContext inputOutputContext = new InputOutputContext(inputStream, fileOutputStream);
+        this.board = new GameBoard(inputOutputContext);
+        computerMover = new ComputerMover(this.board);
+    }
+
+    @After
+    public void teardown() {
+        file.delete();
+    }
+
+    @Test
+    public void shouldFindNextMovePosition() {
+        setBoardCrossOnFirstPosition();
+        assertTrue(computerMover.isValid(0, 0));
+    }
+
+    private void setBoardCrossOnFirstPosition() {
+        this.board.setPosition(0, 0, CellSign.CROSS);
+    }
+
+    private File createTestFile() throws IOException {
+        File file = new File("tmp.bin");
+        if (!file.exists()) {
+            file.createNewFile();
+        }
+        return file;
+    }
+}
